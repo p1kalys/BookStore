301e62e9368d986cef4b7c11a7898d49ec3e8e75
p1kalys <pavanemani14@gmail.com>
2023-10-17 09:09:31 +0530

    Database changes

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
-export const mongoDBURL = `mongodb+srv://gibsongaleo:IQKDMWvYyzWxezMm@bookstore-mern.lsv3byr.mongodb.net/books-collection?retryWrites=true&w=majority`;
+export const mongoDBURL = `ENTER YOUR DATABASE URL`;
 
